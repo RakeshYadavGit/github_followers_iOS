66c7d4b02fe6d8efee99e16f73cacc6e063da251
Rakesh Yadav <rryadav142001@gmail.com>
2025-09-26 16:32:49 +0530

    Update GFAvatarImage.swift

--- GitHubFollowers/GitHubFollowers/CustomComponents/GFAvatarImage.swift
+++ GitHubFollowers/GitHubFollowers/CustomComponents/GFAvatarImage.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class GFAvatarImage: UIImageView {
     
+    let cache: NSCache = NetworkManager.shared.cache
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.configure()
     }
     
     required init?(coder: NSCoder?) {
@@ -24,5 +27,37 @@ class GFAvatarImage: UIImageView {
         self.image              = UIImage(named: "avatar-placeholder")!
         
         
+    }
+    
+    func downloadAvatarImage(avatarUrl: String) {
+        NetworkManager.shared.getAvatarImage(url: avatarUrl) { [weak self] result in
+            
+            guard let self = self else { return }
+            
+            let cacheKey: NSString = NSString(string: avatarUrl)
+            
+            if let image: UIImage = self.cache.object(forKey: cacheKey) {
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+                
+                return
+            }
+            
+            switch result {
+            case .success(let data):
+                
+                guard let image: UIImage = UIImage(data: data) else { return }
+                
+                self.cache.setObject( image,forKey: cacheKey)
+                
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+                
+            case .failure(_):
+                return
+            }
+        }
     }
 }
