65ee692cb3a0e2d01ae07afaa2daf45eb3f18f53
Rakesh Yadav <rryadav142001@gmail.com>
2025-09-27 16:55:16 +0530

    Refactor CustomComponents structure and add loading helpers
    Reorganized CustomComponents into subfolders (GFButton, GFController, GFImageView, GFLabel, GFTextField, GFUIView) and updated project.pbxproj accordingly. Added showLoadView, dismissLoadingView, and showEmptyStateView helper methods to UIViewController extension for improved loading and empty state handling.

--- GitHubFollowers/GitHubFollowers/Extension/UIViewController + Extension.swift
+++ GitHubFollowers/GitHubFollowers/Extension/UIViewController + Extension.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
@@ -19,4 +21,42 @@ extension UIViewController {
             self.present(alertVC, animated: true)
         }
     }
+    
+    func showLoadView() {
+        containerView = UIView(frame: self.view.bounds)
+        self.view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicator: UIActivityIndicatorView = .init(style: .large)
+        containerView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
+        }
+    }
+    
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView: GFEmptyStateView = .init(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+    
+    
 }
